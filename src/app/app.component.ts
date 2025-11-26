236f42a01cfaf9445e53863f4ea922d5e513af4d
ekosta <e.kostandini@forward.al>
2025-11-26 15:21:01 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'application with Angular 17';
+  title = 'application with Angular 17 V2';
 }
+
